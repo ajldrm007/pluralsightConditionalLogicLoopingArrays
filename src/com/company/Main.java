c7bdeb0ca9c361d277f4697a73b2f0b329ecfa61
theone <ajldrm007@gmail.com>
2019-05-13 15:30:44 -0400

    adding conditional statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int v1 = 7;
+        int v2 = 5;
+        int vMax = v1 > v2 ? v1 : v2;
+        System.out.println(vMax);
     }
 }
