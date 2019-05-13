ac347f4990f2b3623afa0b02c7017e49481075d2
theone <ajldrm007@gmail.com>
2019-05-13 15:32:09 -0400

    adding system.out.println conditional statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
         int v1 = 7;
         int v2 = 5;
         int vMax = v1 > v2 ? v1 : v2;
-        System.out.println(vMax);
+        System.out.println("vMax: " + vMax);
     }
 }
