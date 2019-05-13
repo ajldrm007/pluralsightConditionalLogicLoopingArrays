2c00586386a94a932609121682cff7073f4b4100
theone <ajldrm007@gmail.com>
2019-05-13 15:40:26 -0400

    other conditional statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,11 @@ public class Main {
         int v2 = 5;
         int vMax = v1 > v2 ? v1 : v2;
         System.out.println("vMax: " + vMax);
+
+        float students = 30;
+        float rooms = 4;
+
+        float studentsPerRoom = rooms == 0.0f ? 0.0f : students/rooms;
+        System.out.println("Students Per Room: " + studentsPerRoom);
     }
 }
