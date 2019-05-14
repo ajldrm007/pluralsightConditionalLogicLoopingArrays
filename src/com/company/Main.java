6acb0826835b74ca107fa537c5b939b0b129f7c2
theone <ajldrm007@gmail.com>
2019-05-14 18:25:24 -0400

    other conditional statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        /*
         int v1 = 7;
         int v2 = 5;
         int vMax = v1 > v2 ? v1 : v2;
@@ -14,5 +16,37 @@ public class Main {
 
         float studentsPerRoom = rooms == 0.0f ? 0.0f : students/rooms;
         System.out.println("Students Per Room: " + studentsPerRoom);
+
+
+        v1 = 4;
+        v2 = 40;
+        if(v1>v2){System.out.println("v1 is bigger");}
+        else System.out.println("v1 is not bigger");
+
+        v1 = 10;
+        v2 = 40;
+
+        if(v1 > v2)
+            System.out.println("v1 is bigger");
+        else if (v1 < v2)
+            System.out.println("v2 is bigger");
+        else
+            System.out.println("v1 and v2 are equal");
+
+
+         */
+        int v1 = 10, v2 = 4, diff;
+        if(v1>v2){
+            diff = v1-v2;
+            System.out.println("v1 is bigger");
+            System.out.println(diff);
+        }
+        else if (v2>v1){
+            diff = v2 - v1;
+            System.out.println("v2 is bigger");
+            System.out.println(diff);
+        }
+        else
+            System.out.println("v1 and v2 are equal");
     }
 }
